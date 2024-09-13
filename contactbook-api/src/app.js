7ab36ef815f24e6010693836878ce9989aaea910
Tamy <nguyentam080603@gmail.com>
2024-09-13 16:04:48 +0700

    Success

--- contactbook-api/src/app.js
+++ contactbook-api/src/app.js
@@ -6,6 +6,7 @@ const{
     resourceNotFound,
     handleError,
 } = require ('./controllers/errors.controller');
+const { specs, swaggerUi } = require ('./docs/swagger');
 
 const app = express();
 
@@ -18,6 +19,7 @@ return res.json(JSend.success());
 
 contactsRouter.setup(app);
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 app.use('/public', express.static('public'));
 
 //Handle 404 response
